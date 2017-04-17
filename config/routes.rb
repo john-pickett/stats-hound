dba3fd09f1c53d76d7c6a0f4f13edf27d02450d8
John P <john.pickett@gmail.com>
2017-04-17 13:57:37 -0500

    can create events via api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :applications
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 
   root 'welcome#index'
 end
